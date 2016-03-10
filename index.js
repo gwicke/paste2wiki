7011ac0d2b0dca02e5d1a02b1a2a1d3aae8bb879
Gabriel Wicke <gwicke@wikimedia.org>
2016-03-09 20:14:15 -0800

    Move out JS; a couple more clean-ups
    Improve GMail HTML mail support by converting <div>s to <p>s.

---
+++ index.js
@@ -0,0 +1,85 @@
+function reparentChildren(oldNode, newNode) {
+    while (oldNode.childNodes.length > 0) {
+        newNode.appendChild(oldNode.childNodes[0]);
+    }
+}
+
+function renameNode(node, newName) {
+    var newNode = document.createElement(newName);
+    newNode.attributes = node.attributes;
+    node.parentNode.replaceChild(newNode, node);
+    reparentChildren(node, newNode);
+    return newNode;
+}
+
+function removeWrapperNode(node) {
+    while(node.childNodes.length) {
+        node.parentNode.insertBefore(node.childNodes[0], node);
+    }
+    node.parentNode.removeChild(node);
+}
+
+function massageNode(node) {
+    switch(node.nodeName) {
+        case 'STRONG':
+            node = renameNode(node, 'b');
+            break;
+        case 'DIV':
+            node = renameNode(node, 'p');
+            break;
+        case 'SPAN':
+        case 'BR':
+        case 'ASIDE':
+        case 'SECTION':
+        case 'HEADER':
+        case 'TIME':
+            // Replace with its children
+            removeWrapperNode(node);
+            return -1;
+        case 'SVG':
+        case 'svg':
+        case 'IFRAME':
+            // Remove completely
+            node.parentNode.removeChild(node);
+            return -1;
+        case 'A':
+            // Strip links without href
+            if (node.getAttribute('href') === null) {
+                node.parentNode.removeChild(node);
+                return -1;
+            }
+    }
+
+    if (node.nodeType === document.ELEMENT_NODE) {
+        for (var i = 0; i < node.childNodes.length; i++) {
+            i += massageNode(node.childNodes[i]);
+        }
+    } else if (node.nodeType === document.TEXT_NODE) {
+        if (node.parentNode.nodeName !== 'PRE') {
+            // Remove indents outside of preformatted text
+            node.textContent = node.textContent.replace(/(\n|^)[ \t]+/g, '$1');
+        }
+    }
+    return 0;
+}
+
+function massageHTML(node) {
+    for (var i = 0; i < node.childNodes.length; i++) {
+        i += massageNode(node.childNodes[i]);
+    }
+}
+
+function convert() {
+    var data = new FormData();
+    var htmlContentNode = document.getElementById('editable');
+    massageHTML(htmlContentNode);
+    data.append('html', htmlContentNode.innerHTML);
+    data.append('scrub_wikitext', 'true');
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', 'https://en.wikipedia.org/api/rest_v1/transform/html/to/wikitext', true);
+    xhr.onload = function () {
+        // do something to response
+        document.getElementById('output').value = this.responseText
+    };
+    xhr.send(data);
+}
